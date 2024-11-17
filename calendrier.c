c1791b9c139400f9aead7c44e58efc3824e12aeb
Yoann <yoannyo77@gmail.com>
2024-11-17 14:58:49 +0100

    SupprimeEvenementsPeriode QUI MARCHE

--- calendrier.c
+++ calendrier.c
@@ -71,3 +71,53 @@ void AfficheCalendrier(tCalendrier calendrier){
     }
 
 }
+
+int SupprimeEvenementsPeriode(tCalendrier* pCalendrier, struct sDate debut, struct sDate fin){
+    int nbSupprime = 0;
+
+    tCalendrier calActuel = *pCalendrier;
+    tCalendrier* pDernierPSuivantValide = NULL;
+    tCalendrier precedent;
+
+    int estPremierEvenementValide = 1;
+
+    printf("Debut Supprime invalide\n");
+
+    while (calActuel != NULL){
+        printf("Debut boucle while\n");
+        if (Chevauche(Debut(calActuel->pEvenement), Fin(calActuel->pEvenement), debut, fin)){
+            printf("Evenement invalide\n");
+            // Si le sCal est invalide, on le free en utilisant un tCal precedent pour ne pas le perdre
+            DetruitEvenement(&(calActuel->pEvenement));
+            precedent = calActuel;
+            calActuel = calActuel->pSuivant;
+            free(precedent);
+            nbSupprime++;
+
+        } else {
+            printf("Evenement valide\n");
+            if (!estPremierEvenementValide){
+                // On relie le dernier tCal valide avec l'actuel qui est valide
+                *pDernierPSuivantValide = calActuel;
+            } else {
+                *pCalendrier = calActuel;
+            }
+            // On change le dernier pSuivant valide pour celui actuel qui est valide
+            pDernierPSuivantValide = &(calActuel->pSuivant);
+            // Puis on passe au cal d'apres
+            calActuel = calActuel->pSuivant;
+
+            // Nous ne sommes plus au premier evenement valide
+            estPremierEvenementValide = 0;
+
+        }
+
+        // Nous ne sommes plus au premier evenement
+        //estPremierEvenement = 0;
+    }
+
+    // On signe la fin du calendrier en mettant le dernier pSuivant valide a NULL
+    *pDernierPSuivantValide = NULL;
+
+    return nbSupprime;
+}
--- testcalendrier.c
+++ testcalendrier.c
@@ -6,18 +6,40 @@ int main(){
     struct sDate debut1 = {2003, 12, 02, 12, 30};
     struct sDate fin1 = {2003, 12, 02, 15, 00};
     tEvenement anniv = CreerEvenement("Anniv Yoann", debut1, fin1);
+
+    struct sDate debut12 = {2001, 8, 11, 12, 30};
+    struct sDate fin12 = {2001, 8, 11, 15, 00};
+    tEvenement tower = CreerEvenement("Mr. president, there has been a second tower", debut12, fin12);
+
     struct sDate debut2 = {1099, 07, 15, 10, 15};
     struct sDate fin2 = {1099, 07, 16, 21, 25};
     tEvenement jeru = CreerEvenement("Prise de jerusalem par Godefroi", debut2, fin2);
 
-    tCalendrier calendrier = NULL;
-    printf("Avant ajout evenement\n");
-    AjouterEvenement(&calendrier, jeru);
-    printf("Apres ajout evenement 1\n");
-    AfficheCalendrier(calendrier);
-    AjouterEvenement(&calendrier, anniv);
-    printf("Apres ajout evenement 2\n");
+    struct sDate debut3 = {778, 04, 12, 10, 15};
+    struct sDate fin3 = {778, 04, 16, 21, 25};
+    tEvenement charle = CreerEvenement("Charlemagne en Espagne", debut3, fin3);
 
+    tCalendrier calendrier = NULL;
+
+    struct sDate debutSup = {2000, 04, 12, 10, 15};
+    struct sDate finSup = {2005, 04, 12, 10, 15};
+
+    AjouterEvenement(&calendrier, anniv);
+    AjouterEvenement(&calendrier, tower);
+    
+
+    AfficheCalendrier(calendrier);
+    AjouterEvenement(&calendrier, jeru);
+    printf("\n\n");
+
+    AfficheCalendrier(calendrier);
+    AjouterEvenement(&calendrier, charle);
+    printf("\n\n");
+    AfficheCalendrier(calendrier);
+    printf("\n\n");
+
+    SupprimeEvenementsPeriode(&calendrier, debutSup, finSup);
+    printf("\n\n");
     AfficheCalendrier(calendrier);
 
     DetruitCalendrier(&calendrier);
